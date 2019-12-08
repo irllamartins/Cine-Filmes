4061d09e8204ec38717ccb2f64a3af6fd3cb1330
irllamartins <39415559+irllamartins@users.noreply.github.com>
2019-12-07 21:15:15 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+
+function digimon(){
+  window.open('https://www.youtube.com/watch?v=2nHDi5sDJhk')
+}
+function noe(){
+  window.open('https://www.youtube.com/watch?v=VG34gXdqR-4')
+}
+function barbie(){
+  window.open('https://www.youtube.com/watch?v=D-iX-T8d-AY')
+}
+function furia(){
+  window.open('https://www.youtube.com/watch?v=7qgMZfCaChE')
+}
+function rebelion(){
+  window.open('https://www.youtube.com/watch?v=dMgj9q0zMX4&t=8s')
+}
+function tomboy(){
+  window.open('https://www.youtube.com/watch?v=RYLP48Uj4iw')
+}
+function kiminonawa(){
+  window.open('https://www.youtube.com/watch?v=nLhKXh6sJtk')
+}
+function shigatsu(){
+  window.open('https://www.youtube.com/watch?v=FLS4TdJPFVQ')
+}
+function afreira(){
+  window.open('https://www.youtube.com/watch?v=oiH2QeOMrCs')
+}
+function mulheres(){
+  window.open('https://www.youtube.com/watch?v=ZDStZkzW0WI')
+}
+
+$(document).ready(function () {
+  $('[data-toggle="popover"]').popover();
+});
